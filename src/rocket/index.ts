1065def1657826c713468fac2c67e170974d2cf4
breeze9527 <breeze9527@gmail.com>
2021-06-23 21:59:51 +0800

    feat: update Rocket class
    Change Rocket#method, Rocket#responseType, Rocket#timeout
    to private field

--- src/rocket/index.ts
+++ src/rocket/index.ts
@@ -40,14 +40,14 @@ interface RocketOption<D> {
 
 let nextId = 0;
 class Rocket<D, R> {
-  readonly method: HTTPMethod;
-  readonly responseType: ResponseType;
-  readonly source: Source;
-  readonly timeout: number;
+  #method: HTTPMethod;
+  #responseType: ResponseType;
+  #timeout: number;
   #adapter: Adapter;
   #headers: Headers;
   #normalizePayload?: (data: D) => Payload;
   #plugins: RocketPlugin<any>[];
+  #source: Source;
   constructor(options: RocketOption<D>);
   constructor(method: HTTPMethod, source: string | Source);
   constructor(_optionsOrMethod: RocketOption<D> | HTTPMethod, _src?: string | Source) {
@@ -72,18 +72,18 @@ class Rocket<D, R> {
     if (method === undefined) {
       throw new Error('Missing required option: method');
     } else {
-      this.method = options.method;
+      this.#method = options.method;
     }
-    this.responseType = options.responseType ?? 'text';
-    this.timeout = options.timeout ?? 0;
+    this.#responseType = options.responseType ?? 'text';
+    this.#timeout = options.timeout ?? 0;
     if (source === undefined) {
       throw new Error('Missing required option: source');
     } else {
-      this.source = typeof source === 'string' ? Source.from(source) : source;
+      this.#source = typeof source === 'string' ? Source.from(source) : source;
     }
   }
   private normalizePayload(data: D): Payload {
-    const source = this.source;
+    const source = this.#source;
     const normalizer = this.#normalizePayload;
     if (data !== undefined && normalizer === undefined) {
       throw new Error(`Payload normalizer not found: ${source}`);
@@ -108,12 +108,12 @@ class Rocket<D, R> {
       body,
       headers,
       id: id.toString(),
-      method: this.method,
+      method: this.#method,
       params,
       query,
-      responseType: this.responseType,
-      timeout: this.timeout,
-      source: this.source
+      responseType: this.#responseType,
+      timeout: this.#timeout,
+      source: this.#source.clone()
     };
   }
 
